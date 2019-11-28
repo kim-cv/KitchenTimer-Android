0cc8dc8022371b0910a4ec1fb5a1c8e4c90ff595
Kim <kim-cv@kim-cv.com>
2019-11-28 16:03:33 +0100

    no message

--- app/src/main/java/com/funkyqubits/kitchentimer/Repositories/FileSystemRepository.java
+++ app/src/main/java/com/funkyqubits/kitchentimer/Repositories/FileSystemRepository.java
@@ -12,9 +12,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.UUID;
 
 public class FileSystemRepository implements IRepository {
