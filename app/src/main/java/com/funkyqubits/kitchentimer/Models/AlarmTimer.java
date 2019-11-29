4c30da037be1992be3e2ed35e3f6e350a7b7079f
Kim <kim-cv@kim-cv.com>
2019-11-29 15:02:08 +0100

    Added enum for state. Changed how timer progress is calculated. Changed how progress is converted to a readable format.

--- app/src/main/java/com/funkyqubits/kitchentimer/Models/AlarmTimer.java
+++ app/src/main/java/com/funkyqubits/kitchentimer/Models/AlarmTimer.java
@@ -2,51 +2,70 @@ package com.funkyqubits.kitchentimer.Models;
 
 import android.text.format.DateUtils;
 
+import java.util.Calendar;
 import java.util.UUID;
 
 public class AlarmTimer {
+
+    public ALARMTIMER_STATE AlarmTimerState = ALARMTIMER_STATE.NOT_RUNNING;
     public UUID ID;
     public String Title;
     public String ReadableTimer;
-    public boolean IsTimerComplete = false;
     public int LengthInSeconds;
-    private int ProgressInSeconds;
+
+    private long WhenTimerStartedInSeconds;
+    private long SecondsPassedAtTimeOfTick = 0;
+
+    public enum ALARMTIMER_STATE {
+        RUNNING,
+        NOT_RUNNING,
+        PAUSED,
+        COMPLETED
+    }
 
     public AlarmTimer(UUID _id, String _title, int _lengthInSeconds) {
         this.ID = _id;
         this.Title = _title;
         this.LengthInSeconds = _lengthInSeconds;
+        ConvertProgressToReadableTimer();
     }
 
-    public void Start() throws Exception {
-        throw new Exception("Method not implemented yet.");
+    public void Start() {
+        WhenTimerStartedInSeconds = Calendar.getInstance().getTimeInMillis() / 1000;
+        AlarmTimerState = ALARMTIMER_STATE.RUNNING;
     }
 
-    public void Pause() throws Exception {
-        throw new Exception("Method not implemented yet.");
+    public void Pause() {
+        AlarmTimerState = ALARMTIMER_STATE.PAUSED;
     }
 
-    public void Reset() throws Exception {
-        throw new Exception("Method not implemented yet.");
+    public void Reset() {
+        AlarmTimerState = ALARMTIMER_STATE.NOT_RUNNING;
+        WhenTimerStartedInSeconds = -1;
     }
 
     /**
      * Is called each second
      */
     public void Tick() {
-        if (IsTimerComplete) {
+        SecondsPassedAtTimeOfTick = Calendar.getInstance().getTimeInMillis() / 1000;
+        if (AlarmTimerState != ALARMTIMER_STATE.RUNNING) {
             return;
         }
 
-        ProgressInSeconds += 1;
-        this.ReadableTimer = ConvertProgressToReadableTimer();
+        ConvertProgressToReadableTimer();
 
-        if (ProgressInSeconds >= LengthInSeconds) {
-            IsTimerComplete = true;
+        if ((SecondsPassedAtTimeOfTick - WhenTimerStartedInSeconds) >= LengthInSeconds) {
+            AlarmTimerState = ALARMTIMER_STATE.COMPLETED;
         }
     }
 
-    private String ConvertProgressToReadableTimer() {
-        return DateUtils.formatElapsedTime(ProgressInSeconds);
+    private void ConvertProgressToReadableTimer() {
+        long progress = 0;
+        if (WhenTimerStartedInSeconds > 0) {
+            progress = SecondsPassedAtTimeOfTick - WhenTimerStartedInSeconds;
+        }
+
+        this.ReadableTimer = DateUtils.formatElapsedTime(LengthInSeconds - progress);
     }
 }
