f7a156d528bb26e92a5d679705cfb1b67b67699b
Kim <kim-cv@kim-cv.com>
2019-11-29 15:22:31 +0100

    Changed viewholder Changed dataset Added basic timer to call Tick() on running alarmtimers

--- app/src/main/java/com/funkyqubits/kitchentimer/ui/timers/AlarmTimersAdapter.java
+++ app/src/main/java/com/funkyqubits/kitchentimer/ui/timers/AlarmTimersAdapter.java
@@ -1,57 +1,95 @@
 package com.funkyqubits.kitchentimer.ui.timers;
 
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.funkyqubits.kitchentimer.Models.AlarmTimer;
 import com.funkyqubits.kitchentimer.R;
 
-public class AlarmTimersAdapter extends RecyclerView.Adapter<AlarmTimersAdapter.MyViewHolder> {
-    private String[] mDataset;
+import java.util.ArrayList;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class AlarmTimersAdapter extends RecyclerView.Adapter<AlarmTimersAdapter.ViewHolder> {
+    private ArrayList<AlarmTimer> Dataset_alarmTimers;
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
-    public static class MyViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
-        public TextView textView;
-        public MyViewHolder(TextView v) {
+        public LinearLayout LinearLayout;
+
+        public ViewHolder(LinearLayout v) {
             super(v);
-            textView = v;
+            LinearLayout = v;
         }
     }
 
     // Provide a suitable constructor (depends on the kind of dataset)
-    public AlarmTimersAdapter(String[] myDataset) {
-        mDataset = myDataset;
+    public AlarmTimersAdapter(ArrayList<AlarmTimer> alarmTimers) {
+        Dataset_alarmTimers = alarmTimers;
+        InitTimer();
+        Dataset_alarmTimers.get(0).Start();
     }
 
     // Create new views (invoked by the layout manager)
     @Override
-    public AlarmTimersAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent,
-                                                     int viewType) {
+    public ViewHolder onCreateViewHolder(ViewGroup parent,
+                                         int viewType) {
         // create a new view
-        TextView v = (TextView) LayoutInflater.from(parent.getContext())
+        LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.my_text_view, parent, false);
 
-        MyViewHolder vh = new MyViewHolder(v);
+        ViewHolder vh = new ViewHolder(v);
+
         return vh;
     }
 
     // Replace the contents of a view (invoked by the layout manager)
     @Override
-    public void onBindViewHolder(MyViewHolder holder, int position) {
+    public void onBindViewHolder(ViewHolder holder, int position) {
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
-        holder.textView.setText(mDataset[position]);
 
+        TextView txtView_title = holder.LinearLayout.findViewById(R.id.timer_title);
+        TextView txtView_progress = holder.LinearLayout.findViewById(R.id.timer_progress);
+
+        txtView_title.setText(Dataset_alarmTimers.get(position).Title);
+        txtView_progress.setText(Dataset_alarmTimers.get(position).ReadableTimer);
+
+        holder.LinearLayout.setOnClickListener(new TextView.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return mDataset.length;
+        return Dataset_alarmTimers.size();
+    }
+
+    private void InitTimer() {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                for (int i = 0; i < Dataset_alarmTimers.size(); i++) {
+                    AlarmTimer alarmTimer = Dataset_alarmTimers.get(i);
+                    if (alarmTimer.AlarmTimerState == AlarmTimer.ALARMTIMER_STATE.RUNNING) {
+                        alarmTimer.Tick();
+                        notifyItemChanged(i);
+                    }
+                }
+            }
+        }, 0, 1000);//Update every second
     }
 }
