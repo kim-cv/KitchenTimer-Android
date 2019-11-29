3c786055febeb96d45e2dccbdbf57c83ec6231fe
Kim <kim-cv@kim-cv.com>
2019-11-29 15:04:53 +0100

    Added observable list of alarm timers

--- app/src/main/java/com/funkyqubits/kitchentimer/ui/timers/TimersViewModel.java
+++ app/src/main/java/com/funkyqubits/kitchentimer/ui/timers/TimersViewModel.java
@@ -1,30 +1,38 @@
 package com.funkyqubits.kitchentimer.ui.timers;
 
-import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.funkyqubits.kitchentimer.Controller.TimerController;
+import com.funkyqubits.kitchentimer.Models.AlarmTimer;
 import com.funkyqubits.kitchentimer.Repositories.IRepository;
+
+import java.util.ArrayList;
+import java.util.UUID;
 
 public class TimersViewModel extends ViewModel {
 
     private TimerController TimerController;
-    private MutableLiveData<String> mText;
+    public MutableLiveData<ArrayList<AlarmTimer>> alarmTimers = new MutableLiveData<>();
 
     public TimersViewModel() {
-        /*
-        mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
-         */
     }
 
     public void ProvideRepository(IRepository _timerRepository){
         // TODO: Figure out how to use dependency injection in Android MVVM
-        //TimerController = new TimerController(_timerRepository);
+        TimerController = new TimerController(_timerRepository);
+        alarmTimers.setValue(TimerController.GetTimers());
     }
 
-    public LiveData<String> getText() {
-        return mText;
+    public void StartTimer(UUID id) {
+        TimerController.StartTimer(id);
+    }
+
+    public void PauseTimer(UUID id) {
+        TimerController.PauseTimer(id);
+    }
+
+    public void ResetTimer(UUID id) {
+        TimerController.ResetTimer(id);
     }
 }
