1c7db24a85efa0e62f5e97b0b338e1ffbb97ecaa
Kim <kim-cv@kim-cv.com>
2019-12-04 15:07:50 +0100

    Added basic "Add Timer" layout

--- app/src/main/java/com/funkyqubits/kitchentimer/ui/add_timer/AddTimerFragment.java
+++ app/src/main/java/com/funkyqubits/kitchentimer/ui/add_timer/AddTimerFragment.java
@@ -1,18 +1,16 @@
 package com.funkyqubits.kitchentimer.ui.add_timer;
 
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.NumberPicker;
 
 import com.funkyqubits.kitchentimer.R;
 
@@ -25,13 +23,19 @@ public class AddTimerFragment extends Fragment {
         addTimerViewModel =
                 ViewModelProviders.of(this).get(AddTimerViewModel.class);
         View root = inflater.inflate(R.layout.fragment_add_timer, container, false);
-        final TextView textView = root.findViewById(R.id.text_add_timer);
-        addTimerViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        final NumberPicker numberPicker_hours = root.findViewById(R.id.numberPicker_hours);
+        final NumberPicker numberPicker_minutes = root.findViewById(R.id.numberPicker_minutes);
+        final NumberPicker numberPicker_seconds = root.findViewById(R.id.numberPicker_seconds);
+
+        numberPicker_hours.setMinValue(0);
+        numberPicker_hours.setMaxValue(23);
+
+        numberPicker_minutes.setMinValue(0);
+        numberPicker_minutes.setMaxValue(59);
+
+        numberPicker_seconds.setMinValue(0);
+        numberPicker_seconds.setMaxValue(59);
         return root;
     }
 
