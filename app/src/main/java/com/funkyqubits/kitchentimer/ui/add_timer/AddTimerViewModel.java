dacb13062c608c5a12e0a3305d660724cab2fa73
Kim <kim-cv@kim-cv.com>
2019-12-06 11:49:41 +0100

    no message

--- app/src/main/java/com/funkyqubits/kitchentimer/ui/add_timer/AddTimerViewModel.java
+++ app/src/main/java/com/funkyqubits/kitchentimer/ui/add_timer/AddTimerViewModel.java
@@ -1,18 +1,10 @@
 package com.funkyqubits.kitchentimer.ui.add_timer;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class AddTimerViewModel extends ViewModel {
-    private MutableLiveData<String> mText;
 
     public AddTimerViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is add timers fragment");
     }
 
-    public LiveData<String> getText() {
-        return mText;
-    }
 }
