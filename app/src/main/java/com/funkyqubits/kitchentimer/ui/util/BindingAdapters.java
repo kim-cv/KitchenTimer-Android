9f8bc0c4f618842546f33faf779f61de6c389483
Kim <kim-cv@kim-cv.com>
2019-12-14 16:20:26 +0100

    - Renamed UI binding adapter property timerState to timerStateLeftBorder - Added UI binding adapter property timerStateButton to the alarm buttons

--- app/src/main/java/com/funkyqubits/kitchentimer/ui/util/BindingAdapters.java
+++ app/src/main/java/com/funkyqubits/kitchentimer/ui/util/BindingAdapters.java
@@ -1,6 +1,6 @@
 package com.funkyqubits.kitchentimer.ui.util;
 
-import android.graphics.drawable.Drawable;
+import android.content.res.ColorStateList;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.LayerDrawable;
 import android.widget.LinearLayout;
@@ -9,10 +9,129 @@ import androidx.databinding.BindingAdapter;
 
 import com.funkyqubits.kitchentimer.R;
 import com.funkyqubits.kitchentimer.models.AlarmTimer;
+import com.google.android.material.button.MaterialButton;
 
 public class BindingAdapters {
-    @BindingAdapter("timerState")
-    public static void timerState(LinearLayout view, AlarmTimer.ALARMTIMER_STATE alarmtimerState) {
+    @BindingAdapter("timerStateButton")
+    public static void timerStateButton(MaterialButton button, AlarmTimer.ALARMTIMER_STATE alarmtimerState) {
+        int color;
+        ColorStateList colorState;
+
+        switch (button.getId()) {
+            case R.id.btn_timer_start: {
+                color = btn_start_state(button, alarmtimerState);
+                break;
+            }
+            case R.id.btn_timer_pause: {
+                color = btn_pause_state(button, alarmtimerState);
+                break;
+            }
+            case R.id.btn_timer_reset: {
+                color = btn_reset_state(button, alarmtimerState);
+                break;
+            }
+            case R.id.btn_timer_delete: {
+                color = btn_delete_state(button, alarmtimerState);
+                break;
+            }
+            default: {
+                color = button.getResources().getColor(R.color.colorInactive);
+            }
+        }
+        colorState = ColorStateList.valueOf(color);
+        button.setIconTint(colorState);
+    }
+    private static int btn_start_state(MaterialButton button, AlarmTimer.ALARMTIMER_STATE alarmtimerState) {
+        if (alarmtimerState != null) {
+            switch (alarmtimerState) {
+                case NOT_RUNNING: {
+                    return button.getResources().getColor(R.color.colorSuccess);
+                }
+                case PAUSED: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+                case RUNNING: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+                case COMPLETED: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+                default: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+            }
+        }
+        return button.getResources().getColor(R.color.colorInactive);
+    }
+    private static int btn_pause_state(MaterialButton button, AlarmTimer.ALARMTIMER_STATE alarmtimerState) {
+        if (alarmtimerState != null) {
+            switch (alarmtimerState) {
+                case NOT_RUNNING: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+                case PAUSED: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+                case RUNNING: {
+                    return button.getResources().getColor(R.color.colorPrimary);
+                }
+                case COMPLETED: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+                default: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+            }
+        }
+        return button.getResources().getColor(R.color.colorInactive);
+    }
+    private static int btn_reset_state(MaterialButton button, AlarmTimer.ALARMTIMER_STATE alarmtimerState) {
+        if (alarmtimerState != null) {
+            switch (alarmtimerState) {
+                case NOT_RUNNING: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+                case PAUSED: {
+                    return button.getResources().getColor(R.color.colorPrimary);
+                }
+                case RUNNING: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+                case COMPLETED: {
+                    return button.getResources().getColor(R.color.colorPrimary);
+                }
+                default: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+            }
+        }
+        return button.getResources().getColor(R.color.colorInactive);
+    }
+    private static int btn_delete_state(MaterialButton button, AlarmTimer.ALARMTIMER_STATE alarmtimerState) {
+        if (alarmtimerState != null) {
+            switch (alarmtimerState) {
+                case NOT_RUNNING: {
+                    return button.getResources().getColor(R.color.colorDanger);
+                }
+                case PAUSED: {
+                    return button.getResources().getColor(R.color.colorDanger);
+                }
+                case RUNNING: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+                case COMPLETED: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+                default: {
+                    return button.getResources().getColor(R.color.colorInactive);
+                }
+            }
+        }
+        return button.getResources().getColor(R.color.colorInactive);
+    }
+
+    @BindingAdapter("timerStateLeftBorder")
+    public static void timerStateLeftBorder(LinearLayout view, AlarmTimer.ALARMTIMER_STATE alarmtimerState) {
         LayerDrawable layerDrawable = (LayerDrawable) view.getBackground();
 
         GradientDrawable leftBorder = (GradientDrawable) layerDrawable.getDrawable(0);
