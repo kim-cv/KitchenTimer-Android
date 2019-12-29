b94e6ef80eb3c53a6b72084dc84af801d39d078f
Kim <kim-cv@kim-cv.com>
2019-12-29 14:05:47 +0100

    Changed timer complete broadcast receiver to show a notification.

--- app/src/main/java/com/funkyqubits/kitchentimer/BroadcastReceivers/TimerCompleteReceiver.java
+++ app/src/main/java/com/funkyqubits/kitchentimer/BroadcastReceivers/TimerCompleteReceiver.java
@@ -6,13 +6,25 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.funkyqubits.kitchentimer.Controller.NotificationController;
+import com.funkyqubits.kitchentimer.R;
+
 /**
  * Triggers when alarm is complete
  */
 public class TimerCompleteReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("TimerCompleteReceiver", "Alarm Triggered");
-        Toast.makeText(context, "Alarm Triggered", Toast.LENGTH_SHORT).show();
+        NotificationController notificationController = new NotificationController(context);
+
+        String paramIdKey = context.getString(R.string.notifications_parameter_id_key);
+        String paramTitleKey = context.getString(R.string.notifications_parameter_title_key);
+        String paramDescriptionKey = context.getString(R.string.notifications_parameter_description_key);
+
+        int id = intent.getIntExtra(paramIdKey, 0);
+        String title = intent.getStringExtra(paramTitleKey);
+        String description = intent.getStringExtra(paramDescriptionKey);
+        
+        notificationController.ScheduleNotification(id, title, description);
     }
 }
