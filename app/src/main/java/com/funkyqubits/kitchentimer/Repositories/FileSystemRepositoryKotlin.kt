d6fda452d297570e6fb03f50b6a1a68bc8533d17
Kim <kim-cv@kim-cv.com>
2020-01-05 16:50:53 +0100

    Changed to make sure file exist before using it. Changed to make sure file content exist before trying to parse as json.

--- app/src/main/java/com/funkyqubits/kitchentimer/Repositories/FileSystemRepositoryKotlin.kt
+++ app/src/main/java/com/funkyqubits/kitchentimer/Repositories/FileSystemRepositoryKotlin.kt
@@ -13,16 +13,30 @@ class FileSystemRepositoryKotlin(_context: Context, _filename: String) : IFileSy
     private val FileEncoding = Charsets.UTF_8
 
     override fun LoadAlarmTimers(): ArrayList<AlarmTimer> {
-        val file = File(Context.filesDir, Filename)
+        val file = RetrieveFile()
         val fileContent = file.readText(FileEncoding)
-        val gSon = GsonBuilder().registerTypeAdapter(AlarmTimer::class.java, AlarmTimerSerializer.deserializer()).create()
-        return gSon.fromJson<ArrayList<AlarmTimer>>(fileContent, AlarmTimer::class.java)
+
+        val alarmTimers: ArrayList<AlarmTimer> = ArrayList()
+        if (!fileContent.isNullOrEmpty()) {
+            val gSon = GsonBuilder().registerTypeAdapter(AlarmTimer::class.java, AlarmTimerSerializer.deserializer()).create()
+            val tmpAlarmTimers = gSon.fromJson<ArrayList<AlarmTimer>>(fileContent, AlarmTimer::class.java)
+            alarmTimers.addAll(tmpAlarmTimers)
+        }
+        return alarmTimers
     }
 
     override fun SaveAlarmTimers(alarmTimers: ArrayList<AlarmTimer>) {
         val gSon = GsonBuilder().registerTypeAdapter(AlarmTimer::class.java, AlarmTimerSerializer.serializer()).create()
         val json = gSon.toJson(alarmTimers)
-        val file = File(Context.filesDir, Filename)
+        val file = RetrieveFile()
         file.writeText(json, FileEncoding)
+    }
+
+    private fun RetrieveFile(): File {
+        val file = File(Context.filesDir, Filename)
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+        return file
     }
 }
