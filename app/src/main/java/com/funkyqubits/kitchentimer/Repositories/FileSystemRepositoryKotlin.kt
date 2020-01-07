61997916ad01364e4ac5891a5b22da86de7959c9
Kim <kim-cv@kim-cv.com>
2020-01-07 12:47:40 +0100

    Changed to prepare for moving away from Google gson to Kotlin Serialize

--- app/src/main/java/com/funkyqubits/kitchentimer/Repositories/FileSystemRepositoryKotlin.kt
+++ app/src/main/java/com/funkyqubits/kitchentimer/Repositories/FileSystemRepositoryKotlin.kt
@@ -18,19 +18,50 @@ class FileSystemRepositoryKotlin(_context: Context, _filename: String) : IFileSy
 
         val alarmTimers: ArrayList<AlarmTimer> = ArrayList()
         if (!fileContent.isNullOrEmpty()) {
-            val gSon = GsonBuilder().registerTypeAdapter(AlarmTimer::class.java, AlarmTimerSerializer.deserializer()).create()
-            val tmpAlarmTimers = gSon.fromJson<ArrayList<AlarmTimer>>(fileContent, AlarmTimer::class.java)
+            val tmpAlarmTimers = fromJson(false, fileContent)
             alarmTimers.addAll(tmpAlarmTimers)
         }
         return alarmTimers
     }
 
     override fun SaveAlarmTimers(alarmTimers: ArrayList<AlarmTimer>) {
-        val gSon = GsonBuilder().registerTypeAdapter(AlarmTimer::class.java, AlarmTimerSerializer.serializer()).create()
-        val json = gSon.toJson(alarmTimers)
+        val json = toJson(false, alarmTimers)
         val file = RetrieveFile()
         file.writeText(json, FileEncoding)
     }
+
+
+    private fun fromJson(useKotlinSerializable: Boolean, json: String): ArrayList<AlarmTimer> {
+        if (useKotlinSerializable) {
+            throw NotImplementedError()
+        } else {
+            return gsonFromJson(json)
+        }
+    }
+
+    private fun toJson(useKotlinSerializable: Boolean, alarmTimers: ArrayList<AlarmTimer>): String {
+        if (useKotlinSerializable) {
+            throw NotImplementedError()
+        } else {
+            return gsonToJson(alarmTimers)
+        }
+    }
+
+    //#region Kotlin serialize
+    //TODO Change from Google Gson to Kotlin Serialize, require rewrite of AlarmTimer to kotlin
+    //#endregion
+
+    //#region Gson
+    private fun gsonFromJson(json: String): ArrayList<AlarmTimer> {
+        val gSon = GsonBuilder().registerTypeAdapter(AlarmTimer::class.java, AlarmTimerSerializer.deserializer()).create()
+        return gSon.fromJson<ArrayList<AlarmTimer>>(json, AlarmTimer::class.java)
+    }
+
+    private fun gsonToJson(alarmTimers: ArrayList<AlarmTimer>): String {
+        val gSon = GsonBuilder().registerTypeAdapter(AlarmTimer::class.java, AlarmTimerSerializer.serializer()).create()
+        return gSon.toJson(alarmTimers)
+    }
+    //#endregion
 
     private fun RetrieveFile(): File {
         val file = File(Context.filesDir, Filename)
