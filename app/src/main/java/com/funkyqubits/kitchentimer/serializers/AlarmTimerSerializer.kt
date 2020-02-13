c32d02bf6d341dd484b8b56cb47a2c1b4c542f4a
Kim <kim-cv@kim-cv.com>
2020-02-13 15:01:52 +0100

    Improved gson json deserializer

--- app/src/main/java/com/funkyqubits/kitchentimer/serializers/AlarmTimerSerializer.kt
+++ app/src/main/java/com/funkyqubits/kitchentimer/serializers/AlarmTimerSerializer.kt
@@ -1,7 +1,10 @@
 package com.funkyqubits.kitchentimer.serializers
 
 import com.funkyqubits.kitchentimer.models.AlarmTimer
-import com.google.gson.*
+import com.google.gson.JsonArray
+import com.google.gson.JsonDeserializer
+import com.google.gson.JsonObject
+import com.google.gson.JsonSerializer
 
 class AlarmTimerSerializer {
     companion object {
@@ -25,9 +28,31 @@ class AlarmTimerSerializer {
                 json.forEach {
                     it as JsonObject
 
-                    val id = it.get("id").asInt
-                    val title = it.get("title").asString
-                    val lengthInSeconds = it.get("time").asInt
+                    val idJsonElement = it.get("id")
+                    val titleJsonElement = it.get("title")
+                    val lengthInSecondsJsonElement = it.get("time")
+
+                    val id: Int
+                    val title: String
+                    val lengthInSeconds: Int
+
+                    // Make sure we got ID
+                    id = when (idJsonElement == null) {
+                        true -> -1
+                        false -> idJsonElement.asInt
+                    }
+
+                    // Make sure we got title
+                    title = when (titleJsonElement == null) {
+                        true -> "No title"
+                        false -> titleJsonElement.asString
+                    }
+
+                    // Make sure we got time
+                    lengthInSeconds = when (lengthInSecondsJsonElement == null) {
+                        true -> 60
+                        false -> lengthInSecondsJsonElement.asInt
+                    }
 
                     val alarmTimer = AlarmTimer(id, title, lengthInSeconds, AlarmTimer.ALARMTIMER_SAVE_TYPE.SAVE)
                     alarmTimers.add(alarmTimer)
