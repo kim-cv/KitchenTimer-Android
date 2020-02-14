7b40c255989f3b7c9610b772b90504719c191070
Kim <kim-cv@kim-cv.com>
2020-02-14 11:59:38 +0100

    Improved custom JSON serializer to be backwards compatible with old app data format.

--- app/src/main/java/com/funkyqubits/kitchentimer/serializers/AlarmTimerSerializer.kt
+++ app/src/main/java/com/funkyqubits/kitchentimer/serializers/AlarmTimerSerializer.kt
@@ -1,10 +1,7 @@
 package com.funkyqubits.kitchentimer.serializers
 
 import com.funkyqubits.kitchentimer.models.AlarmTimer
-import com.google.gson.JsonArray
-import com.google.gson.JsonDeserializer
-import com.google.gson.JsonObject
-import com.google.gson.JsonSerializer
+import com.google.gson.*
 
 class AlarmTimerSerializer {
     companion object {
@@ -51,7 +48,7 @@ class AlarmTimerSerializer {
                     // Make sure we got time
                     lengthInSeconds = when (lengthInSecondsJsonElement == null) {
                         true -> 60
-                        false -> lengthInSecondsJsonElement.asInt
+                        false -> convertLengthFormat(lengthInSecondsJsonElement)
                     }
 
                     val alarmTimer = AlarmTimer(id, title, lengthInSeconds, AlarmTimer.ALARMTIMER_SAVE_TYPE.SAVE)
@@ -60,6 +57,34 @@ class AlarmTimerSerializer {
 
                 alarmTimers
             }
+        }
+
+        private fun convertLengthFormat (lengthInSecondsJsonElement: JsonElement): Int {
+            val asString = lengthInSecondsJsonElement.asString
+
+            // Check if old time format
+            if (Regex("^\\d{2}:\\d{2}:\\d{2}$").matches(asString)) {
+                // Is old format
+                return mapOldLengthFormatToNewFormat(asString)
+            } else {
+                return lengthInSecondsJsonElement.asInt
+            }
+        }
+
+        private fun mapOldLengthFormatToNewFormat(oldFormat: String): Int {
+            val oldFormatParts = oldFormat.split(':')
+
+            val hoursString = oldFormatParts[0]
+            val minutesString = oldFormatParts[1]
+            val secondsString = oldFormatParts[2]
+
+            val hours = Integer.parseInt(hoursString)
+            val minutes = Integer.parseInt(minutesString)
+            val seconds = Integer.parseInt(secondsString)
+
+            val timeLength = (hours * 60 * 60) + (minutes * 60) + seconds
+
+            return timeLength
         }
     }
 }
