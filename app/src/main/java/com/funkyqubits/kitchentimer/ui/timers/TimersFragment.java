82f848b8163d8239bf3be204a4cb424fbc9d48ba
Kim <kim-cv@kim-cv.com>
2020-05-06 11:12:29 +0200

    no message

--- app/src/main/java/com/funkyqubits/kitchentimer/ui/timers/TimersFragment.java
+++ app/src/main/java/com/funkyqubits/kitchentimer/ui/timers/TimersFragment.java
@@ -142,32 +142,14 @@ public class TimersFragment extends Fragment implements IAlarmTimerUIEventsObser
     //#region Events for each alarm timer
     @Override
     public void OnAlarmTimerStarted(int alarmTimerID) {
-        AlarmTimer alarmTimer = TimersViewModel.FindTimerOnId(alarmTimerID);
-        if (alarmTimer == null) {
-            return;
-        }
-
-        AlarmAudioService.Companion.addRunningTimer(getContext(), alarmTimer.ID, alarmTimer.Title);
     }
 
     @Override
     public void OnAlarmTimerResumed(int alarmTimerID) {
-        AlarmTimer alarmTimer = TimersViewModel.FindTimerOnId(alarmTimerID);
-        if (alarmTimer == null) {
-            return;
-        }
-
-        AlarmAudioService.Companion.addRunningTimer(getContext(), alarmTimer.ID, alarmTimer.Title);
     }
 
     @Override
     public void OnAlarmTimerPaused(int alarmTimerID) {
-        AlarmTimer alarmTimer = TimersViewModel.FindTimerOnId(alarmTimerID);
-        if (alarmTimer == null) {
-            return;
-        }
-
-        AlarmAudioService.Companion.removeRunningTimer(getContext(), alarmTimer.ID, alarmTimer.Title);
     }
 
     @Override
